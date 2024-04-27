9f89aa2a6aabc12db0af99638b2b804eeb252682
Piotr387 <ptrpiw.dev@gmail.com>
2024-04-27 17:17:30 +0200

    Initial Commit

---
+++ ZaawansowaneProgramowanieSwift/ZaawansowaneProgramowanieSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaawansowaneProgramowanieSwiftApp.swift
+//  ZaawansowaneProgramowanieSwift
+//
+//  Created by Piotr Piwonski on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZaawansowaneProgramowanieSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
